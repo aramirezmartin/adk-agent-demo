a27291cffb98f6f927fb49255658edb459399177
anramar <anramar@google.com>
2025-05-15 21:50:14 +0000

    Set URL to get apikey

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -54,7 +54,7 @@
       # Runs when the workspace is (re)started
       onStart = {
         # Example: start a background task to watch and re-build backend code
-        # watch-backend = "npm run watch-backend";
+        watch-backend = "source .venv/bin/activate";
       };
     };
   };
